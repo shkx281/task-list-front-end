398f78b43c841f705ca0650cdbcf38a74d99746d
Kelly Kang <shkx281@gmail.com>
2022-06-20 16:17:02 -0700

    Completed Wave 2. Uplifted State for Toggle complete feature and delete feature.

--- src/App.js
+++ src/App.js
@@ -1,28 +1,66 @@
 import React from 'react';
 import TaskList from './components/TaskList.js';
 import './App.css';
+import { useState } from 'react';
 
-const TASKS = [
-  {
-    id: 1,
-    title: 'Mow the lawn',
-    isComplete: false,
-  },
-  {
-    id: 2,
-    title: 'Cook Pasta',
-    isComplete: true,
-  },
-];
+// const TASKS = [
+//   {
+//     id: 1,
+//     title: 'Mow the lawn',
+//     isComplete: false,
+//   },
+//   {
+//     id: 2,
+//     title: 'Cook Pasta',
+//     isComplete: true,
+//   },
+// ];
 
 const App = () => {
+  const [tasks, setTasks] = useState([
+    {
+      id: 1,
+      title: 'Mow the lawn',
+      isComplete: false,
+    },
+    {
+      id: 2,
+      title: 'Cook Pasta',
+      isComplete: true,
+    },
+  ]);
+  const flipComplete = (id) => {
+    const newTasks = [];
+    for (const task of tasks) {
+      if (task.id === id) {
+        task.isComplete = !task.isComplete;
+      }
+      newTasks.push(task);
+    }
+    setTasks(newTasks);
+  };
+  const deleteTask = (id) => {
+    const newTasks = [];
+    for (const task of tasks) {
+      if (task.id !== id) {
+        newTasks.push(task);
+      }
+    }
+    setTasks(newTasks);
+  };
   return (
     <div className="App">
       <header className="App-header">
         <h1>Ada&apos;s Task List</h1>
       </header>
       <main>
-        <div>{<TaskList tasks={TASKS} />}</div>
+        <div>
+          <TaskList
+            tasks={tasks}
+            completeCallback={flipComplete}
+            deleteCallback={deleteTask}
+          />
+        </div>
       </main>
     </div>
   );
--- src/components/Task.js
+++ src/components/Task.js
@@ -1,21 +1,31 @@
-import React, { useState } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 
 import './Task.css';
 
 const Task = (props) => {
-  const [complete, setComplete] = useState(props.isComplete);
-  const buttonClass = complete ? 'tasks__item__toggle--completed' : '';
+  // const [complete, setComplete] = useState(props.isComplete);
+  const buttonClass = props.isComplete ? 'tasks__item__toggle--completed' : '';
+
+  const flipComplete = () => {
+    props.completeCallback(props.id);
+  };
+
+  const deleteTask = () => {
+    props.deleteCallback(props.id);
+  };
 
   return (
     <li className="tasks__item">
       <button
         className={`tasks__item__toggle ${buttonClass}`}
-        onClick={() => setComplete(!complete)}
+        onClick={flipComplete}
       >
         {props.title}
       </button>
-      <button className="tasks__item__remove button">x</button>
+      <button className="tasks__item__remove button" onClick={deleteTask}>
+        x
+      </button>
     </li>
   );
 };
@@ -24,6 +34,8 @@ Task.propTypes = {
   id: PropTypes.number.isRequired,
   title: PropTypes.string.isRequired,
   isComplete: PropTypes.bool.isRequired,
+  completeCallback: PropTypes.func.isRequired,
+  deleteCallback: PropTypes.func.isRequired,
 };
 
 export default Task;
