1446a986c6a0e272437b9ebad60e71f845d5a6ca
maria <camilatagleo@gmail.com>
2022-06-24 10:35:07 -0700

    working on Wave 4

---
+++ src/components/NewTaskForm.js
@@ -0,0 +1,46 @@
+import { useState } from 'react';
+
+const defaultTask = {
+  title: '',
+  isComplete: false,
+  description: '',
+};
+
+const TaskForm = (props) => {
+  const [formData, setFormData] = useState(defaultTask);
+
+  const onFormChange = (event) => {
+    const stateName = event.target.name;
+    const inputValue = event.target.value;
+
+    const newFormData = { ...formData };
+    newFormData[stateName] = inputValue;
+
+    setFormData(newFormData);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    props.addTask(formData);
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label htmlFor="title">Title</label>
+      <input
+        type="text"
+        name="title"
+        value={formData.title}
+        onChange={onFormChange}
+      />
+      <label htmlFor="description">Description</label>
+      <input
+        type="text"
+        name="description"
+        value={formData.description}
+        onChange={onFormChange}
+      />
+      <input type="submit" value="Add Task" />
+    </form>
+  );
+};
